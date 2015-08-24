b47897f2ee363abce7d9b0f7b95089a1008337f8
mrohrhirs <manuel.rohrhirs@gmail.com>
2015-08-24 20:05:48 +0200

    Init Overview

--- public/app/app.js
+++ public/app/app.js
@@ -1,1 +1,33 @@
-angular.module('MathTrainer', ['app.routes']);
+
+var app = angular.module('MathTrainer', ['app.routes','ngTouch', 'ui.grid']);
+
+
+        app.controller('MainController', ['$scope', function ($scope) {
+
+            $scope.myData = [
+                {
+                    "creater": "Peter",
+                    "createDate": "12.05.2015",
+                    "difficulty": 4,
+                    "myScore": 3,
+                    "myTime" : "00:12:34",
+                    "myLastExecution" : "12.05.2015"
+                },
+                {
+                    "creater": "Paul",
+                    "createDate": "12.05.2015",
+                    "difficulty": 4,
+                    "myScore": 3,
+                    "myTime" : "00:12:34",
+                    "myLastExecution" : "12.05.2015"
+                },
+                {
+                    "creater": "Marry",
+                    "createDate": "12.05.2015",
+                    "difficulty": 4,
+                    "myScore": 3,
+                    "myTime" : "00:12:34",
+                    "myLastExecution" : "12.05.2015"
+                }
+            ];
+        }]);
