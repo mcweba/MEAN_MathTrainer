fa3e06de6dd1a41177823232eb4eb9cbd24e0dfd
mrohrhirs <manuel.rohrhirs@gmail.com>
2015-08-24 21:33:00 +0200

    UID Column visable = false

--- public/app/app.js
+++ public/app/app.js
@@ -3,9 +3,11 @@ var app = angular.module('MathTrainer', ['app.routes','ngTouch', 'ui.grid']);
 
 
         app.controller('MainController', ['$scope', function ($scope) {
+            $scope.mySelections = [];
 
-            $scope.myData = [
+            var data = [
                 {
+                    "UID":"8D568A36-D583-42E7-A441-EE4F05592741",
                     "creater": "Peter",
                     "createDate": "12.05.2015",
                     "difficulty": 4,
@@ -14,6 +16,7 @@ var app = angular.module('MathTrainer', ['app.routes','ngTouch', 'ui.grid']);
                     "myLastExecution" : "12.05.2015"
                 },
                 {
+                    "UID":"8D568A36-D583-42E7-A441-EE4F05592741",
                     "creater": "Paul",
                     "createDate": "12.05.2015",
                     "difficulty": 4,
@@ -22,6 +25,7 @@ var app = angular.module('MathTrainer', ['app.routes','ngTouch', 'ui.grid']);
                     "myLastExecution" : "12.05.2015"
                 },
                 {
+                    "UID":"8D568A36-D583-42E7-A441-EE4F05592741",
                     "creater": "Marry",
                     "createDate": "12.05.2015",
                     "difficulty": 4,
@@ -30,4 +34,19 @@ var app = angular.module('MathTrainer', ['app.routes','ngTouch', 'ui.grid']);
                     "myLastExecution" : "12.05.2015"
                 }
             ];
+
+           var def = [{ field: 'UID', displayName: 'UID', visible: false },
+               { field: 'creater'},
+               { field: 'createDate'},
+               { field: 'difficulty'},
+               { field: 'myScore'},
+               { field: 'myTime'},
+               { field: 'myLastExecution'}
+           ];
+
+            $scope.gridOptions = {
+                data: data,
+                columnDefs :  def
+            };
+
         }]);
