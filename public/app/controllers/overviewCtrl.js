785303b783f5b139f4c3b9f7052e5d926f36fb05
mrohrhirs <manuel.rohrhirs@gmail.com>
2015-08-27 21:57:30 +0200

    filter hizugefügt

--- public/app/controllers/overviewCtrl.js
+++ public/app/controllers/overviewCtrl.js
@@ -18,19 +18,19 @@ angular.module('overviewCtrl', ['ngTouch', 'ui.grid'])
             {
                 "UID":"8D568A36-D583-42E7-A441-EE4F05592741",
                 "creater": "Paul",
-                "createDate": "12.05.2015",
-                "difficulty": 4,
-                "myScore": 3,
-                "myTime" : "00:12:34",
-                "myLastExecution" : "12.05.2015"
+                "createDate": "11.07.2014",
+                "difficulty": 2,
+                "myScore": 1,
+                "myTime" : "01:12:34",
+                "myLastExecution" : "12.07.2015"
             },
             {
                 "UID":"8D568A36-D583-42E7-A441-EE4F05592741",
                 "creater": "Marry",
-                "createDate": "12.05.2015",
-                "difficulty": 4,
-                "myScore": 3,
-                "myTime" : "00:12:34",
+                "createDate": "02.08.2005",
+                "difficulty": 9,
+                "myScore": 8,
+                "myTime" : "10:12:34",
                 "myLastExecution" : "12.05.2015"
             }
         ];
@@ -47,10 +47,11 @@ angular.module('overviewCtrl', ['ngTouch', 'ui.grid'])
             { field: 'myScore'},
             { field: 'myTime'},
             { field: 'myLastExecution'},
-            {name:'Link ',cellTemplate:'<div style="margin: 0px" ><button class="glyphicon glyphicon-plus" ng-click="grid.appScope.overview.editRow(grid, row)"></button></div>'}
+            { sortable: false, enableSorting : false,  enableFiltering: false,  name:'Link ',cellTemplate:'<div style="margin: 0px" ><button class="glyphicon glyphicon-plus" ng-click="grid.appScope.overview.editRow(grid, row)"></button></div>'}
         ];
 
         vm.gridOptions = {
+            enableFiltering: true,
             data: data,
             columnDefs :  def
         };
