0bed0f79167b7857eaf8f6d39c17acaf9f4c3f54
mrohrhirs <manuel.rohrhirs@gmail.com>
2015-09-01 09:48:21 +0200

    filter hizugefügt

--- public/app/controllers/overviewCtrl.js
+++ public/app/controllers/overviewCtrl.js
@@ -1,58 +1,90 @@
-angular.module('mathApp.overview', ['ngTouch', 'ui.grid'])
+angular.module('mathApp.overview', ['ngTouch', 'ui.grid', 'angular-clipboard'])
 
-    .controller('overviewController', [function() {
+    .controller('overviewController', [function () {
         var vm = this;
         vm.test = 888;
         vm.mySelections = [];
 
         var data = [
             {
-                "UID":"8D568A36-D583-42E7-A441-EE4F05592741",
+                "UID": "8D568A36-D583-42E7-A441-EE4F05592741",
                 "creater": "Peter",
                 "createDate": "12.05.2015",
                 "difficulty": 4,
                 "myScore": 3,
-                "myTime" : "00:12:34",
-                "myLastExecution" : "12.05.2015"
+                "myTime": "00:12:34",
+                "myLastExecution": "12.05.2015"
             },
             {
-                "UID":"8D568A36-D583-42E7-A441-EE4F05592741",
+                "UID": "8D568A36-D583-42E7-A441-EE4F05592741",
                 "creater": "Paul",
                 "createDate": "11.07.2014",
                 "difficulty": 2,
                 "myScore": 1,
-                "myTime" : "01:12:34",
-                "myLastExecution" : "12.07.2015"
+                "myTime": "01:12:34",
+                "myLastExecution": "12.07.2015"
             },
             {
-                "UID":"8D568A36-D583-42E7-A441-EE4F05592741",
+                "UID": "8D568A36-D583-42E7-A441-EE4F05592741",
                 "creater": "Marry",
                 "createDate": "02.08.2005",
                 "difficulty": 9,
                 "myScore": 8,
-                "myTime" : "10:12:34",
-                "myLastExecution" : "12.05.2015"
+                "myTime": "10:12:34",
+                "myLastExecution": "12.05.2015"
             }
         ];
 
         vm.editRow = function (grid, row) {
+            vm.textToCopy = 'I can copy by clicking!';
+            var selectedUID = row.entity.UID;
+        };
 
+        vm.success = function () {
+            console.log('Copied!');
+        };
+
+        vm.fail = function (err) {
+            console.error('Error!', err);
+        };
+
+        vm.startTraining = function (grid, row) {
         };
 
         var def = [
-            { field: 'UID', displayName: 'UID', visible: false },
-            { field: 'creater'},
-            { field: 'createDate'},
-            { field: 'difficulty'},
-            { field: 'myScore'},
-            { field: 'myTime'},
-            { field: 'myLastExecution'},
-            { sortable: false, enableSorting : false,  enableFiltering: false,  name:'Link ',cellTemplate:'<div style="margin: 0px" ><button class="glyphicon glyphicon-plus" ng-click="grid.appScope.overview.editRow(grid, row)"></button></div>'}
+            {field: 'UID', displayName: 'UID', visible: false, enableHiding: false},
+            {displayName: 'Erzeuger', field: 'creater', enableHiding: false},
+            {displayName: 'Erzeugungsdatum', field: 'createDate', enableHiding: false},
+            {displayName: 'Schwierigkeitsgrad', field: 'difficulty', enableHiding: false},
+            {displayName: 'Meine erreichte Punktzahl', field: 'myScore', enableHiding: false},
+            {displayName: 'Dauer', field: 'myTime', enableHiding: false},
+            {displayName: 'Zuletzt am', field: 'myLastExecution', enableHiding: false},
+            {
+                displayName: 'Link kopieren',
+                enableColumnMenu: false,
+                sortable: false,
+                enableSorting: false,
+                enableFiltering: false,
+                name: 'Link ',
+                cellTemplate: '<div class="text-center" style="margin: 0px" ><button clipboard on-copied="grid.appScope.overview.success()" on-error="grid.appScope.overview.fail(err)" class="glyphicon glyphicon-plus" ng-click="grid.appScope.overview.editRow(grid, row)"></button></div>',
+                enableHiding: false
+            },
+            {
+                displayName: 'Starten',
+                enableColumnMenu: false,
+                sortable: false,
+                enableSorting: false,
+                enableFiltering: false,
+                name: 'Loesen ',
+                cellTemplate: '<div  class="text-center" style="margin: 0px" ><button class="glyphicon glyphicon-hourglass" ng-click="grid.appScope.overview.editRow(grid, row)"></button></div>',
+                enableHiding: false
+            }
         ];
 
         vm.gridOptions = {
+            enableHiding: false,
             enableFiltering: true,
             data: data,
-            columnDefs :  def
+            columnDefs: def
         };
     }]);
