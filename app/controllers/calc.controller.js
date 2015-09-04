d8564c74416a0b4e99a4a20ab29ecb0ab64ec566
Marc-André Weber <maweber84@gmail.com>
2015-09-04 15:57:56 +0000

    calc.controller.js edited online with Bitbucket

--- app/controllers/calc.controller.js
+++ app/controllers/calc.controller.js
@@ -1,10 +1,11 @@
 var Calculation = require('../models/calculation');
+var CalculationSet = require('../models/calculationset');
 var _ = require('lodash');
 var Q = require('q');
 
 exports.create = function(req, res) {
 
-    // prepare promises
+    // prepare promises to get or create calculations
     var promises = [];
     _(req.body.calculations).forEach(function(calculation){
         promises.push(createOrGetCalculationId(calculation.n1, calculation.op, calculation.n2, calculation.res));
@@ -12,21 +13,42 @@ exports.create = function(req, res) {
 
 
     // wait for all promises to resolve or reject
-    Q.all(promises).then(function(result){
-        console.log('done with all promises:' + result.length);
-        res.json({ message: 'calcset received'});
+    Q.all(promises).then(function(calculations){
+        saveCalculationSet(req.decoded.userId, req.body.diff_level, calculations).then(function(){
+            res.json({ message: 'calculationset successfully created'});    
+        }).catch(function(error){
+            res.status(400).send({message: error.message});
+        });
     }).catch(function(error){
         res.status(400).send({message: error.message});
     });
 
 };
 
-exports.get = function(req, res) {
+exports.get = function(req, res) {    
     var data = [];
     data.push({id: 1234, creator: 'Fritz', created: new Date(), diff_level: 1, score: 99, duration: 240, lastExec: new Date()});
     data.push({id: 1235, creator: 'Hans', created: new Date(), diff_level: 3, score: 55, duration: 600, lastExec: new Date()});
     data.push({id: 1236, creator: 'Kurt', created: new Date(), diff_level: 2, score: 66, duration: 90, lastExec: new Date()});
     res.json(data);
+};
+
+var saveCalculationSet = function(creatorId, diff_level, calculations){
+    var deferred = Q.defer();
+    
+    var calculationSet = new CalculationSet();
+    calculationSet.creator = creatorId;
+    calculationSet.diff_level = diff_level;
+    calculationSet.calculations = calculations;
+        
+    calculationSet.save(function(err){
+        if(err){
+            deferred.reject(new Error(error));
+        } else {
+            deferred.resolve();
+        }
+    });
+    return deferred.promise;
 };
 
 var createOrGetCalculationId = function(num1, op, num2, result){
