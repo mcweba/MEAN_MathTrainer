edf61c66e1fd997311674f8d3b4ec5408654d2f4
Marc-André Weber <maweber84@gmail.com>
2015-09-04 15:55:42 +0000

    calculationset.js created online with Bitbucket

---
+++ app/models/calculationset.js
@@ -0,0 +1,20 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var CalculationSetSchema  = new Schema({
+	created: {
+		type: Date,
+		default: Date.now
+	},
+    creator: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	},
+    diff_level: {
+        type: Number,
+        required: 'diff level is required'
+    },
+    calculations: [{type: mongoose.Schema.Types.ObjectId, ref: 'Calculation'}]
+});
+
+module.exports = mongoose.model('CalculationSet', CalculationSetSchema);
