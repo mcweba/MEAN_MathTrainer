86cdefdeb144f7d68805770e0e031dc98e4a53d4
skottmann@bitbucket.org <stko81@msn.com>
2015-09-10 21:38:19 +0200

    solveCtrl: umleitung bei fehlerhaftem calcset

--- public/app/controllers/solveCtrl.js
+++ public/app/controllers/solveCtrl.js
@@ -21,103 +21,110 @@ angular.module('mathApp.solve', ['ui.bootstrap'])
             var vm = this;
 
             vm.currentCalcSet = CalcService.getCurrentCalcSet();
-            if(vm.currentCalcSet === undefined){
-                $modalInstance.close();
-                $location.path('/overview');
-               // return;
-            }
+            if(vm.currentCalcSet === undefined || vm.currentCalcSet === null){
+                vm.undefinedCalc = true;
+                vm.ok = function () {
+                    $modalInstance.close();
+                    $location.path('/overview');
+                };
+            }else {
 
-            vm.calcCount = vm.currentCalcSet.calculations.length;
+                vm.calcCount = vm.currentCalcSet.calculations.length;
 
 
-            vm.beforestart = true;
-            vm.started = false;
-            vm.finished = false;
-            vm.progress = false;
-            vm.currentResult = '';
-            vm.calculationSolves = [];
-            vm.duration = 0;
-            vm.durationTime = 0;
-            vm.score = 0;
+                vm.beforestart = true;
+                vm.started = false;
+                vm.finished = false;
+                vm.progress = false;
+                vm.undefinedCalc = false;
+                vm.currentResult = '';
+                vm.calculationSolves = [];
+                vm.duration = 0;
+                vm.durationTime = 0;
+                vm.score = 0;
 
-            vm.currentCalcIndex = 1;
+                vm.currentCalcIndex = 1;
 
-            vm.startCalc = function(){
-                vm.started = true;
-                vm.progress = true;
-                vm.beforestart = false;
-                vm.start = new Date().getTime();
-            };
+                vm.startCalc = function () {
+                    vm.started = true;
+                    vm.progress = true;
+                    vm.beforestart = false;
+                    vm.start = new Date().getTime();
+                };
 
-            vm.currentNb1 = function(){
-                if (vm.currentCalcIndex <  vm.calcCount + 1) {
-                    return vm.currentCalcSet.calculations[vm.currentCalcIndex - 1].number1;
-                }
-            };
-
-            vm.currentNb2 = function(){
-                if (vm.currentCalcIndex <  vm.calcCount + 1) {
-                    return vm.currentCalcSet.calculations[vm.currentCalcIndex - 1].number2;
-                }
-            };
-
-            vm.currentOp = function(){
-                if (vm.currentCalcIndex <  vm.calcCount + 1) {
-                    return vm.currentCalcSet.calculations[vm.currentCalcIndex - 1].operator;
-                }
-            };
-
-            vm.addCalc = function(calcId, providedRes, duration, correct){
-                vm.calculationSolves.push({
-                        "calcId": calcId,
-                        "providedRes": providedRes,
-                        "duration": duration,
-                        "correct": correct
+                vm.currentNb1 = function () {
+                    if (vm.currentCalcIndex < vm.calcCount + 1) {
+                        return vm.currentCalcSet.calculations[vm.currentCalcIndex - 1].number1;
                     }
-                )
-            };
+                };
 
-            vm.nextCalc = function(keyEvent){
-                if (keyEvent.which === 13) {
-                    if (vm.currentCalcIndex <  vm.calcCount + 1) {
-                        var id = vm.currentCalcSet.calculations[vm.currentCalcIndex-1]._id;
-                        var isCalcTrue = vm.currentCalcSet.calculations[vm.currentCalcIndex-1].result.toString() === vm.currentResult;
-                        var calcTime = new Date().getTime() - vm.start;
-                        vm.duration += calcTime;
-                        if(isCalcTrue){vm.score +=1;}
-                        vm.addCalc(id, vm.currentResult, calcTime, isCalcTrue);
-                        vm.currentCalcIndex += 1;
-                        console.log('Result: ' +  vm.currentResult);
-                        vm.currentResult = '';
-                        vm.start = new Date().getTime();
-                        if (vm.currentCalcIndex  ===  vm.calcCount + 1)
-                        {
-                            vm.started = false;
-                            vm.finished = true;
-                            vm.currentCalcIndex -=1;
-                            vm.durationTime = Math.floor(vm.duration /1000) + ' Sekunden';
+                vm.currentNb2 = function () {
+                    if (vm.currentCalcIndex < vm.calcCount + 1) {
+                        return vm.currentCalcSet.calculations[vm.currentCalcIndex - 1].number2;
+                    }
+                };
+
+                vm.currentOp = function () {
+                    if (vm.currentCalcIndex < vm.calcCount + 1) {
+                        return vm.currentCalcSet.calculations[vm.currentCalcIndex - 1].operator;
+                    }
+                };
+
+                vm.addCalc = function (calcId, providedRes, duration, correct) {
+                    vm.calculationSolves.push({
+                            "calcId": calcId,
+                            "providedRes": providedRes,
+                            "duration": duration,
+                            "correct": correct
                         }
-                    } else {
-                        vm.ok();
-                    }
-                }
-            };
+                    )
+                };
 
-            vm.ok = function () {
-                var result ={
-                    "score": vm.score,
-                    "duration": vm.duration,
-                    "calculationset": vm.currentCalcSet._id,
-                    "calculationSolves": [vm.calculationSolves
-                ]};
-                CalcService.submitCalcSetSolve(result);
-                // console.log('submitCalcSetSolve: ' + result);
-                $modalInstance.close();
-                $location.path('/overview');
-            };
-            vm.cancel = function () {
-                $modalInstance.dismiss();
-                $location.path('/overview');
-            };
+                vm.nextCalc = function (keyEvent) {
+                    if (keyEvent.which === 13) {
+                        if (vm.currentCalcIndex < vm.calcCount + 1) {
+                            var id = vm.currentCalcSet.calculations[vm.currentCalcIndex - 1]._id;
+                            var isCalcTrue = vm.currentCalcSet.calculations[vm.currentCalcIndex - 1].result.toString() === vm.currentResult;
+                            var calcTime = new Date().getTime() - vm.start;
+                            vm.duration += calcTime;
+                            if (isCalcTrue) {
+                                vm.score += 1;
+                            }
+                            vm.addCalc(id, vm.currentResult, calcTime, isCalcTrue);
+                            vm.currentCalcIndex += 1;
+                            console.log('Result: ' + vm.currentResult);
+                            vm.currentResult = '';
+                            vm.start = new Date().getTime();
+                            if (vm.currentCalcIndex === vm.calcCount + 1) {
+                                vm.started = false;
+                                vm.finished = true;
+                                vm.currentCalcIndex -= 1;
+                                vm.durationTime = Math.floor(vm.duration / 1000) + ' Sekunden';
+                            }
+                        } else {
+                            vm.ok();
+                        }
+                    }
+                };
+
+                vm.ok = function () {
+                    var result = {
+                        "score": vm.score,
+                        "duration": vm.duration,
+                        "calculationset": vm.currentCalcSet._id,
+                        "calculationSolves": [vm.calculationSolves
+                        ]
+                    };
+                    CalcService.submitCalcSetSolve(result);
+                    // console.log('submitCalcSetSolve: ' + result);
+                    $modalInstance.close();
+                    $location.path('/overview');
+                };
+
+                vm.cancel = function () {
+                    $modalInstance.dismiss();
+                    $location.path('/overview');
+                };
+            }
         }
     }]);
