7d257a96165bd8980294dc41a00433fd509ce292
Marc-André Weber <maweber84@gmail.com>
2015-09-10 20:22:16 +0200

    added calculationsetinfo model

---
+++ app/models/calculationsetinfo.js
@@ -0,0 +1,23 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var CalculationSetInfoSchema  = new Schema({
+    creator: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	},
+	calculationset: {
+		type: Schema.ObjectId,
+		ref: 'CalculationSet'
+	},
+    lastscore: {
+        type: Number,
+        required: 'last score is required'
+    },
+	lastsolve: {
+		type: Date,
+		required: 'last solve is required'
+	}
+});
+
+module.exports = mongoose.model('CalculationSetInfo', CalculationSetInfoSchema);
