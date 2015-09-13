009da9605099a4ccb415f28dd0b39f7a0a49e6b8
mrohrhirs <manuel.rohrhirs@gmail.com>
2015-09-13 13:20:21 +0200

    value currentUser added

--- public/app/app.js
+++ public/app/app.js
@@ -13,7 +13,7 @@ angular.module('mathApp', [
     .constant('dateTimeSourceFormat', 'YYYY-MM-DD HH:mm:ss.SSS')
     .constant('dateTimeTargetFormat', 'DD.MM.YYYY HH:mm')
     .constant('dateTargetFormat', 'DD.MM.YYYY')
-    .value('currentUser', {token: "", id: "777"})
+    .value('currentUser', { name: {}, userId: {}, role: {}})
     .config(function ($httpProvider) {
         // attach auth interceptor to http requests
         $httpProvider.interceptors.push('AuthInterceptor');
